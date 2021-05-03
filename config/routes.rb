dc70c2ddda119bd1fda9bc2cac4c6863365fde2b
Daisuke Maruyama <maruyama.d8@gmail.com>
2021-05-03 20:15:46 +0900

    [Update]Devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,20 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/about' => 'homes#about'
 
+    #devise関連
+  devise_for :users, :controllers => {
+    :registrations => 'devise/users/registrations',
+    :sessions => 'devise/users/sessions',
+    :passwords => 'devise/users/passwords'
+  }
+
+  devise_scope :user do
+    get "user/:id", :to => "users/registrations#detail"
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+    get "password", :to => "users/passwords#new"
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
