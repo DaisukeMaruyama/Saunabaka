8714e5c03116e67caaff47d0c9fffbfc93ebcc49
Daisuke Maruyama <maruyama.d8@gmail.com>
2021-05-18 17:39:44 +0900

    [Update]Forumルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
       
       resources :facilities, only: [:index, :show, :edit, :new, :create, :destroy]
     
+      resources :forums, only: [:index, :show, :edit, :new, :create, :destroy]
     end
     
     namespace :admin do
