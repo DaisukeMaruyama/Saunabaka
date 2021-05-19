3e593c0a0ff75e94ca1d5b0b5d2c1d21c3bb38df
Daisuke Maruyama <maruyama.d8@gmail.com>
2021-05-19 13:43:55 +0900

    [Update]コントローラー処理追加

--- app/controllers/public/forums_controller.rb
+++ app/controllers/public/forums_controller.rb
@@ -1,12 +1,22 @@
 class Public::ForumsController < ApplicationController
 
   def index
+    @forums = Forum.all
   end
 
   def new
+    @forum = Forum.new
   end
 
   def create
+    @forum = Forum.create(forum_params)
+    @forum.user_id = current_user.id
+    if @forum.save
+      flash[:notice] = "投稿が完了しました。"
+      redirect_to forums_path
+    else
+      render :new
+    end   
   end
 
   def show
@@ -16,6 +26,12 @@ class Public::ForumsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def forum_params
+    params.require(:forum).permit(:message, :forum_image)
   end
 
 end
