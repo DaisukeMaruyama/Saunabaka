88eba388101e892d5ab751dd41507a4a49e87f55
Daisuke Maruyama <maruyama.d8@gmail.com>
2021-05-20 09:16:16 +0900

    [Update]コントローラー中身記述

--- app/controllers/public/topics_controller.rb
+++ app/controllers/public/topics_controller.rb
@@ -1,12 +1,22 @@
 class Public::TopicsController < ApplicationController
   
   def index
+    @topics = Topic.all
   end
 
   def show
+    @topic = @topic.find(params[:id])
   end
 
   def create
+    @topic = Topic.new(topic_params)
+    @topic.user_id = current_user.id
+    if @topic.save
+      flash[:notice] = "新規トピックを作成しました。"
+      redirect_to topics_path
+    else
+      render :index
+    end
   end
 
   def destroy
@@ -14,4 +24,11 @@ class Public::TopicsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:title)
+  end
+
 end
