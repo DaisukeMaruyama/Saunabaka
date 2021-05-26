bc242ed0dc0675e22ab7b1846e839b906056cd7e
Daisuke Maruyama <maruyama.d8@gmail.com>
2021-05-26 10:20:02 +0900

    [Update]おすすめサウナ投稿アクション作成

--- app/controllers/public/recommendations_controller.rb
+++ app/controllers/public/recommendations_controller.rb
@@ -9,9 +9,17 @@ class Public::RecommendationsController < ApplicationController
   end
 
   def new
+    @recommendation = Recommendation.new
   end
 
   def create
+    @recommendation = Recommendation.create(recommendation_params)
+    if @recomendation.save
+      flash[:notice] = "おすすめサウナを投稿しました。"
+      redirect_to recommendations_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,4 +27,22 @@ class Public::RecommendationsController < ApplicationController
 
   def destroy
   end
+
+  def recommendation_params
+    params.require(:recomendation).permit(
+      :image,
+			:sauna_name,
+      :temperature,
+      :water,
+      :prefecture,
+      :postal_code,
+      :address,
+      :working_hours,
+      :phone_number,
+      :price_description,
+      :hp,
+      :parking
+    )
+  end
+
 end
