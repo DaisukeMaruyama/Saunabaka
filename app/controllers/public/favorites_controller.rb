4250f258466f7aef83046d5ea4b0e82c9915f95f
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-06-20 19:19:53 +0900

    facilityの情報を受け取るアクション追加

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,5 +1,6 @@
 class Public::FavoritesController < ApplicationController
 
+  before_action :set_facility
 
   def create
     if @facility.user_id != current_user.id 
@@ -9,6 +10,10 @@ class Public::FavoritesController < ApplicationController
   end
 
   def destory
+  end
+
+  def set_facility
+    @facility = Facility.find(params[:id])
   end
 
 end
