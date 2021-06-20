17e515d209345325f18337ad0e5b6a7821481fc2
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-06-20 18:58:21 +0900

    [Add]お気に入りテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_053403) do
+ActiveRecord::Schema.define(version: 2021_06_20_095610) do
 
   create_table "facilities", force: :cascade do |t|
     t.string "sauna_name"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 2021_05_24_053403) do
     t.boolean "parking"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "facility_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["facility_id"], name: "index_favorites_on_facility_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "forums", force: :cascade do |t|
@@ -77,4 +86,6 @@ ActiveRecord::Schema.define(version: 2021_05_24_053403) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "facilities"
+  add_foreign_key "favorites", "users"
 end
