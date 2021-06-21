90d3e4054c7dc2ed6c32642f4d3a6742769431ec
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-06-21 11:42:38 +0900

    インデント修正

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,11 +1,11 @@
 class Facility < ApplicationRecord
 
-	has_many :favorites , dependent: :destroy
-	attachment :image
+  has_many :favorites , dependent: :destroy
+  attachment :image
 
-	enum parking:{有: true, 無: false}
+  enum parking:{有: true, 無: false}
 
-	def favorited_by?(current_user)
+  def favorited_by?(current_user)
     favorites.where(user_id: current_user.id).exists?
   end
 
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,8 +2,6 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :facility
 
-
-
   def favorited_by?(current_user)
     favorites.where(user_id: current_user.id).exists?
   end
