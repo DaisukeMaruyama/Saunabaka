145dab0ac633e5897bbbac58fb12ce8c3f56c011
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-06-21 11:36:58 +0900

    [Update]お気に入りイイね機能作成

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,22 +1,22 @@
 class Public::FavoritesController < ApplicationController
-
+  before_action :authenticate_user!
   before_action :set_facility
 
   def create
-    if @facility.user_id != current_user.id 
+    #if @facility.user_id != current_user.id 
       Favorite.create(user_id: current_user.id, facility_id: @facility.id)
       flash[:notice] = "お気に入りに追加しました。"
-    end
+    #end
   end
 
-  def destory
+  def destroy
     @favorite = Favorite.find_by(user_id: current_user.id, facility_id: @facility.id)
-    @facility.destory
+    @favorite.destroy
     flash[:notice] = "お気に入りを解除しました。"
   end
 
   def set_facility
-    @facility = Facility.find(params[:id])
+    @facility = Facility.find(params[:facility_id])
   end
 
 end
