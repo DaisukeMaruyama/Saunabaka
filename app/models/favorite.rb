3fe561b9d1ce5809657b16112347b42ef44fbe72
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-06-21 13:38:49 +0900

    [Update]facilityモデルの記述だけで問題なし

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,9 +1,5 @@
 class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :facility
-
-  def favorited_by?(current_user)
-    favorites.where(user_id: current_user.id).exists?
-  end
   
 end
