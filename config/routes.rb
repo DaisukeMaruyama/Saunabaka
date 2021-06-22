5ba3295864d5e8c7cda9405d82c379e2e3f460b1
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-06-22 09:28:30 +0900

    [Update]userルーティング編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/withdraw'
-  get 'users/unsubscribe'
-  get 'favorites/create'
-  get 'favorites/destory'
   #devise関連
     devise_for :users, :controllers => {
       :registrations => 'devise/users/registrations',
@@ -33,7 +26,7 @@ Rails.application.routes.draw do
         resource :favorites, only: [:create, :destroy]
       end
 
-      resources :users, only: [:show, :edit, :update] do
+      resources :users, only: [:show, :edit, :update, :withdraw, :unsubscribe] do
         get :favorites, on: :collection
       end
     
