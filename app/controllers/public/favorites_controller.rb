83d48d52c59432a8413091b4aecc29d2e11f4b19
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-06-23 09:13:39 +0900

    [Update]マイページからお気にり削除機能作成

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -15,6 +15,13 @@ class Public::FavoritesController < ApplicationController
     flash[:notice] = "お気に入りを解除しました。"
   end
 
+  def userFavoriteDelete
+    @favorite = Favorite.find_by(user_id: current_user.id, facility_id: @facility.id)
+    @favorite.destroy
+    flash[:notice] = "お気に入りを解除しました。"
+    redirect_to user_path(current_user.id)
+  end
+
   def set_facility
     @facility = Facility.find(params[:facility_id])
   end
