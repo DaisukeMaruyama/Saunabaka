4494a4958670b49be854f0f28867ed7b04d60faf
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-06-28 15:16:15 +0900

    [Update]退会後はログインできないようにする。

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  enum is_deleted:{Deleted: true, Nondeleted: false}
+
+  #is_deletedされていない（false）ならログイン可能
+  def active_for_authentication?
+    super && (self.is_deleted == "Nondeleted")
+  end       
+
   has_many :forums, dependent: :destroy
   has_many :topics, dependent: :destroy
   has_many :recommendations, dependent: :destroy
