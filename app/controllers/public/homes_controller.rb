275af2190d7c8e0f964d76f17c1a18c69072488a
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-07-04 16:51:01 +0900

    [Update]いいねが多い順に並び替え

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
 
   def top
-    @facilities = Facility.all.order(created_at: :desc).limit(3)
+    @facilities = Facility.find(Favorite.group(:facility_id).order('count(facility_id) desc').limit(3).pluck(:facility_id))
   end
 
   def about
