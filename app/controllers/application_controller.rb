c6c9fa238f66452238c52a250ad338a0086b0772
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-07-07 10:56:02 +0900

    [Update]ransackエラー防ぐ記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :set_search
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  #これを定義しないとNo Ransack::Search object was provided to search_form_for!エラーが出る
+  def set_search
+    @q = Facility.ransack(params[:q])
+    @facilities = @q.result(distinct: :true).order(created_at: :desc)
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email,:name])
