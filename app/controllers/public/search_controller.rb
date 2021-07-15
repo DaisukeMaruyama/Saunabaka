09b03707bfe2f62c0eb58f8a1483035519df01f2
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-07-15 10:59:29 +0900

    [Update]ヘッダー検索機能実装

--- app/controllers/public/search_controller.rb
+++ app/controllers/public/search_controller.rb
@@ -1,6 +1,14 @@
 class Public::SearchController < ApplicationController
 
   def search
+    @value = params["search"]["value"]
+    @facilities = search_for_item(@value).order(created_at: :desc)
+  end
+
+  private
+
+  def search_for_item(value)
+    Facility.where("sauna_name LIKE ?", "%#{value}%")
   end
 
 end
