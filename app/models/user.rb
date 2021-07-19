e55b10a862c5a21fce1d2b4328e3546d5be60236
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-07-19 09:36:00 +0900

    [Update]バリデーションメールフォーマット指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
 
 
   # Include default devise modules. Others available are:
