b9e452265254e3e5d366bbf7a0ce3660a0f8deb1
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-07-24 13:35:29 +0900

    [Update]コメントアウト追記

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,5 +1,6 @@
 RailsAdmin.config do |config|
 
+  #rails_admin認証
   config.authenticate_with do
     authenticate_or_request_with_http_basic('Site Message') do |username, password|
       username == 'admin' && password == 'admin'
