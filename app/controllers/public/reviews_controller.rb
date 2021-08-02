74dca9313ecae64535e610cd0baf84f8e4f48644
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-08-02 15:22:41 +0900

    [Update]レビュー投稿のためアクション記述

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,10 +1,21 @@
 class Public::ReviewsController < ApplicationController
+  before_action :find_facility
 
   def new
     @review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.facility_id = @facility.id
+    @review.user_id = current_user.id
+
+    if @review.save
+      redirect_to facility_path(@facility)
+    else
+      render :new
+    end
+
   end
 
   def edit
@@ -14,6 +25,16 @@ class Public::ReviewsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rate, :comment)
+  end
+
+  def find_facility
+    @facility = Facility.find(params[:facility_id])
   end
 
 end
