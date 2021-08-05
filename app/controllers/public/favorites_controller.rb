c98226362106d7ac686577b6c27de13ca46fceed
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-08-05 10:52:36 +0900

    [Update]不要なコメントアウト削除

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -3,10 +3,8 @@ class Public::FavoritesController < ApplicationController
   before_action :set_facility
 
   def create
-    #if @facility.user_id != current_user.id 
       Favorite.create(user_id: current_user.id, facility_id: @facility.id)
       flash[:notice] = "お気に入りに追加しました。"
-    #end
   end
 
   def destroy
