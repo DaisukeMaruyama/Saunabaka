8bbe66001562e612d5b0047e3eb953d3ebe24074
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-08-16 14:35:41 +0900

    [Update]バリデーション追記

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,4 +1,18 @@
 class Recommendation < ApplicationRecord
+
+	validates :sauna_name, presence: true
+  validates :prefecture, presence: true
+  validates :phone_number, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :temperature, presence: true, length: { maximum: 3 }  
+  validates :water, presence: true, length: { maximum: 2 }  
+  validates :price_description, presence: true
+  validates :working_hours, presence: true
+  validates :hp, presence: true
+  validates :parking, presence: true
+  validates :image, presence: true
+
 	attachment :image
 	belongs_to :user
 end
