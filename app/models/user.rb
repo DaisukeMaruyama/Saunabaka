53514925444f0bdacf934039f596cfdd6c2a7b1d
DaisukeMaruyama <maruyama.d8@gmail.com>
2021-08-23 14:11:47 +0900

    [Update]インデント修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
 
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
